a2c5e11149c756af1782bf9d5c20a635dcd6ad6e
lishumin1314 <1742388380@qq.com>
2021-05-05 13:04:41 +0800

    demo

--- springcloud-eureka-feign-mybatis-seata/order-server/src/main/java/io/seata/sample/service/OrderServiceImpl.java
+++ springcloud-eureka-feign-mybatis-seata/order-server/src/main/java/io/seata/sample/service/OrderServiceImpl.java
@@ -5,7 +5,9 @@ import io.seata.sample.entity.Order;
 import io.seata.sample.feign.AccountApi;
 import io.seata.sample.feign.StorageApi;
 import io.seata.spring.annotation.GlobalTransactional;
+
 import java.math.BigDecimal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,7 @@ import org.springframework.stereotype.Service;
  * @author IT云清
  */
 @Service("orderServiceImpl")
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
 
@@ -28,26 +30,26 @@ public class OrderServiceImpl implements OrderService{
 
     /**
      * 创建订单
+     *
      * @param order
-     * @return
-     * 测试结果：
+     * @return 测试结果：
      * 1.添加本地事务：仅仅扣减库存
      * 2.不添加本地事务：创建订单，扣减库存
      */
     @Override
-    @GlobalTransactional(name = "fsp-create-order",rollbackFor = Exception.class)
+    @GlobalTransactional(name = "fsp-create-order", rollbackFor = Exception.class)
     public void create(Order order) {
         LOGGER.info("------->交易开始");
         //本地方法
         orderDao.create(order);
 
         //远程方法 扣减库存
-        storageApi.decrease(order.getProductId(),order.getCount());
+        storageApi.decrease(order.getProductId(), order.getCount());
 
         //远程方法 扣减账户余额
 
         LOGGER.info("------->扣减账户开始order中");
-        accountApi.decrease(order.getUserId(),order.getMoney());
+        accountApi.decrease(order.getUserId(), order.getMoney());
         LOGGER.info("------->扣减账户结束order中");
 
         LOGGER.info("------->交易结束");
@@ -57,8 +59,8 @@ public class OrderServiceImpl implements OrderService{
      * 修改订单状态
      */
     @Override
-    public void update(Long userId,BigDecimal money,Integer status) {
-        LOGGER.info("修改订单状态，入参为：userId={},money={},status={}",userId,money,status);
-        orderDao.update(userId,money,status);
+    public void update(Long userId, BigDecimal money, Integer status) {
+        LOGGER.info("修改订单状态，入参为：userId={},money={},status={}", userId, money, status);
+        orderDao.update(userId, money, status);
     }
 }
